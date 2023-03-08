4deeb0a102eb887e2bf1ac144356a6354060751d
anandvechalapu <anand.vechalapu@gmail.com>
2023-03-08 11:46:17 +0530

    initial commit

---
+++ Forget password/Reset password 0.6751591325804972.java
@@ -0,0 +1,11 @@
+
+
+1. Go to the website’s login page.
+2. Click on the “Forgot Password” link.
+3. Enter your registered email address.
+4. Click “Submit” or “Send”.
+5. Check your email for a reset password link.
+6. Click the link and enter a new password.
+7. Confirm your new password.
+8. Click “Submit” or “Reset Password”.
+9. Login to your account with your new password.
